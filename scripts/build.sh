c2b84ac2cb20fbacf8fd1b68421fe0dd768d273e
gongweibao <weibao.gong@gmail.com>
2020-04-29 03:38:30 +0000

    clean up

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,11 +4,16 @@ unset GREP_OPTIONS
 BASEDIR=$(dirname $(readlink -f $0))
 
 cd ${BASEDIR}/..
+
+pushd python/paddle_edl/protos/
+python run_codegen.py
+popd
+
 build_dir=build
 mkdir -p  ${build_dir}
 cd ${build_dir}
 
 cmake ..
 make clean && make -j
-pip install paddlepaddle-gpu
+python -m pip install paddlepaddle-gpu
 ctest -V -R
