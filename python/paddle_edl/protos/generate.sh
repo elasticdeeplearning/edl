b2bd6c07bd67a532fae71047dd224d2b5302833e
gongweibao <weibao.gong@gmail.com>
2020-04-29 09:32:19 +0000

    cleanup

---
+++ python/paddle_edl/protos/generate.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -xe
+
+if [[ ! -d "/tmp/protoc" ]]; then
+    mkdir -p /tmp/protoc 
+    pushd /tmp/protoc
+    wget -O protoc-3.11.4-linux-x86_64.zip  --no-check-certificate  https://github.com/protocolbuffers/protobuf/releases/download/v3.11.4/protoc-3.11.4-linux-x86_64.zip
+    unzip protoc-3.11.4-linux-x86_64.zip
+    popd
+fi
+
+export PATH=/tmp/protoc/bin:$PATH
+
+protoc --go_out=plugins=grpc:./  master.proto
+protoc --go_out=plugins=grpc:./  common.proto
+
+mv *.go ../../../pkg/master/
+python ./run_codegen.py
+ 
