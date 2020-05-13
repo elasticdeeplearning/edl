32724d65d011ffa7f512e6657c17adc9e8775dee
gongweibao <weibao.gong@gmail.com>
2020-05-13 08:48:46 +0000

    add

--- docker/build-example-demo.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-image=hub.baidubce.com/paddle-edl/paddle_edl:latest-cuda10.0-cudnn7-example-demo
-
-docker build --network host . -t ${image} -f docker/Dockerfile.example-demo
-docker push ${image}
