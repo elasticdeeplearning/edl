63f5759b021ff779c2a8d64c9e1088ddcd820985
gongweibao <weibao.gong@gmail.com>
2020-05-15 06:11:38 +0000

    fix download etcd test=develop

--- scripts/download_etcd.sh
+++ scripts/download_etcd.sh
@@ -3,14 +3,12 @@ set -e
 ETCD_VER=v3.4.7
 
 # choose either URL
-GOOGLE_URL=https://storage.googleapis.com/etcd
-GITHUB_URL=https://github.com/etcd-io/etcd/releases/download
-DOWNLOAD_URL=${GOOGLE_URL}
+DOWNLOAD_URL=https://paddle-edl.bj.bcebos.com/etcd-${ETCD_VER}-linux-amd64.tar.gz
 
 rm -f /tmp/etcd-${ETCD_VER}-linux-amd64.tar.gz
 rm -rf /tmp/etcd-download-test && mkdir -p /tmp/etcd-download-test
 
-wget -q ${DOWNLOAD_URL}/${ETCD_VER}/etcd-${ETCD_VER}-linux-amd64.tar.gz -O /tmp/etcd-${ETCD_VER}-linux-amd64.tar.gz
+wget -q ${DOWNLOAD_URL} -O /tmp/etcd-${ETCD_VER}-linux-amd64.tar.gz
 tar xzvf /tmp/etcd-${ETCD_VER}-linux-amd64.tar.gz -C /tmp/etcd-download-test --strip-components=1
 rm -f /tmp/etcd-${ETCD_VER}-linux-amd64.tar.gz
 
