e2fc042dcc5412499d2c23a88b7dcb86144eba37
gongweibao <weibao.gong@gmail.com>
2020-05-15 07:05:24 +0000

    add test=develop

--- python/paddle_edl/protos/generate.sh
+++ python/paddle_edl/protos/generate.sh
@@ -1,19 +1,6 @@
 #!/bin/bash
 set -xe
 
-if [[ ! -d "/tmp/protoc" ]]; then
-    mkdir -p /tmp/protoc 
-    pushd /tmp/protoc
-    wget -O protoc-3.11.4-linux-x86_64.zip  --no-check-certificate  https://github.com/protocolbuffers/protobuf/releases/download/v3.11.4/protoc-3.11.4-linux-x86_64.zip
-    unzip protoc-3.11.4-linux-x86_64.zip
-    popd
-fi
-
-pushd  /tmp/
-go get -u -v github.com/golang/protobuf/protoc-gen-go@v1.3.0
-popd
-export PATH=/tmp/protoc/bin:$PATH:$(go env GOPATH)
-
 protoc --go_out=plugins=grpc:./  master.proto
 protoc --go_out=plugins=grpc:./  common.proto
 
