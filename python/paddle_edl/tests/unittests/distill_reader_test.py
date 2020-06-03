2ac863434c53a640ae5da89a5962be3112eea7ee
WangXi <wangxi16@baidu.com>
2020-06-03 12:04:36 +0800

    remove print, test=develop
    Signed-off-by: WangXi <wangxi16@baidu.com>

--- python/paddle_edl/tests/unittests/distill_reader_test.py
+++ python/paddle_edl/tests/unittests/distill_reader_test.py
@@ -14,37 +14,41 @@
 
 import numpy as np
 import paddle_edl.distill.distill_reader as distill_reader
+import unittest
+
+
+class TestDistillReader(unittest.TestCase):
+    def test_distill_reader(self):
+        # temp local test
+        distill_reader.distill_worker._NOP_PREDICT_TEST = True
+
+        # test mnist distill reader
+        def _reader():
+            img = np.array(
+                [(i + 1) / 28.0 for i in range(28)] * 28,
+                dtype=np.float32).reshape((1, 28, 28))
+            label = np.array([100], dtype=np.int64)
+            for i in range(24):
+                yield 8 * [(img, label)]
+            yield 2 * [(img, label)]
+
+        dr = distill_reader.DistillReader(
+            ins=['img', 'label'], predicts=['prediction'])
+        dr.set_teacher_batch_size(4)
+        dr.set_fixed_teacher(['127.0.0.1:9292', '127.0.0.1:9293'])
+        # dr.set_dynamic_teacher(['127.0.0.1:7001'], 'DistillReaderTest', 3)
+
+        train_reader = dr.set_sample_list_generator(_reader)
+        dr.print_config()
+
+        for epoch in range(300):
+            for step, batch in enumerate(train_reader()):
+                if epoch == 0 and step == 0:
+                    dr.print_config()
+            if epoch % 10 == 0:
+                print('^^^^^^^^^^^^^ epoch={} predict[0][0]={}^^^^^^^^^^^^^^'.
+                      format(epoch, batch[-1][-1][0]))
+
 
 if __name__ == '__main__':
-    # temp local test
-    distill_reader.distill_worker._NOP_PREDICT_TEST = True
-
-    # test mnist distill reader
-    def _reader():
-        img = np.array(
-            [(i + 1) / 28.0 for i in range(28)] * 28,
-            dtype=np.float32).reshape((1, 28, 28))
-        label = np.array([100], dtype=np.int64)
-        for i in range(24):
-            yield 8 * [(img, label)]
-        yield 2 * [(img, label)]
-
-    dr = distill_reader.DistillReader(
-        ins=['img', 'label'], predicts=['prediction'])
-    dr.set_teacher_batch_size(4)
-    dr.set_fixed_teacher(['127.0.0.1:9292', '127.0.0.1:9293'])
-    # dr.set_dynamic_teacher(['127.0.0.1:7001'], 'DistillReaderTest', 3)
-
-    train_reader = dr.set_sample_list_generator(_reader)
-    dr.print_config()
-
-    for epoch in range(300):
-        for step, batch in enumerate(train_reader()):
-            if epoch == 0 and step == 0:
-                dr.print_config()
-            print('----step={}, predict_shape={}, predict[0]={} ----'.format(
-                step, len(batch), batch[-1][-1]))
-            pass
-        if epoch % 10 == 0:
-            print('^^^^^^^^^^^^^ epoch={} predict[0][0]={}^^^^^^^^^^^^^^'.
-                  format(epoch, batch[-1][-1][0]))
+    unittest.main()
