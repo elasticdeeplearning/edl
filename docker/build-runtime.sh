137f4fb7d05d38b68e0beb5bf7a7103ffa37b58f
gongweibao <weibao.gong@gmail.com>
2020-06-08 03:16:58 +0000

    add more test=develop

--- docker/build-runtime.sh
+++ docker/build-runtime.sh
@@ -15,6 +15,8 @@ latest_image=hub.baidubce.com/paddle-edl/paddle_edl:latest-cuda10.0-cudnn7
 docker build --network host . -t ${latest_image} -f docker/Dockerfile.runtime
 docker push ${latest_image}
 
+version=$1
+
 version_image=hub.baidubce.com/paddle-edl/paddle_edl:${version}-cuda10.0-cudnn7
 docker tag ${latest_image} ${version_image}
 docker push ${version_image}
