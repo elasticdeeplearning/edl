58abeea86b2911eb1b989fe437dd0d087b1c4061
gongweibao <weibao.gong@gmail.com>
2020-06-10 10:15:49 +0800

    Refine README of EDL. (#117)

--- docker/build-runtime.sh
+++ docker/build-runtime.sh
@@ -10,13 +10,24 @@ unset GREP_OPTIONS
 BASEDIR=$(dirname $(readlink -f $0))
 cd ${BASEDIR}/..
 
-latest_image=hub.baidubce.com/paddle-edl/paddle_edl:latest-cuda10.0-cudnn7
+build_image(){
+    cuda_version=$1
+    latest_image=hub.baidubce.com/paddle-edl/paddle_edl:latest-cuda${cuda_version}-cudnn7
+    sed 's/<baseimg>/1.8.0-gpu-cuda'"${cuda_version}"'-cudnn7/g' docker/Dockerfile.runtime > docker/Dockerfile.runtime.cuda${cuda_version}
+    docker build --network host . -t ${latest_image} -f docker/Dockerfile.runtime.cuda${cuda_version}
+    docker push ${latest_image}
 
-docker build --network host . -t ${latest_image} -f docker/Dockerfile.runtime
-docker push ${latest_image}
+    version=$2
+    version_image=hub.baidubce.com/paddle-edl/paddle_edl:${version}-cuda${cuda_version}-cudnn7
+    docker tag ${latest_image} ${version_image}
+    docker push ${version_image}
+}
 
 version=$1
+cuda_version="10.0"
+echo "build cuda:${cuda_version} edl version:${version}"
+build_image ${cuda_version} $version
 
-version_image=hub.baidubce.com/paddle-edl/paddle_edl:${version}-cuda10.0-cudnn7
-docker tag ${latest_image} ${version_image}
-docker push ${version_image}
+cuda_version="9.0"
+echo "build cuda:${cuda_version} edl version:${version}"
+build_image ${cuda_version} $version
