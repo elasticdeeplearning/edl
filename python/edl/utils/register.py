1754aaa1cb1259d82d35a7f0d4943d0d2b172ad5
gongweibao <weibao.gong@gmail.com>
2020-08-18 06:40:08 +0000

    fix test=develop

--- python/edl/utils/register.py
+++ python/edl/utils/register.py
@@ -14,6 +14,7 @@
 import threading
 import time
 import json
+import uuid
 
 from .utils import logger
 from .cluster import Pod, PodStatus
@@ -29,7 +30,7 @@ class Register(object):
         self._server = server
         self._stop = threading.Event()
         #don't change this ttl
-        self._etcd = EtcdClient(etcd_endpoints, root=job_id)
+        self._etcd = EtcdClient(endpoints=etcd_endpoints, root=job_id)
         self._etcd.init()
 
         try:
