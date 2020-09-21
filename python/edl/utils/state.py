328a5b7b4ed7a3c10f7d0334014e372a71c79c16
gongweibao <weibao.gong@gmail.com>
2020-09-21 11:40:57 +0800

    Add unittest of state (#143)

--- python/edl/utils/state.py
+++ python/edl/utils/state.py
@@ -12,86 +12,110 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import json
-from edl.discovery.etcd_client import EtcdClient
+import six
 from edl.utils import constants
 from edl.utils import error_utils
 from edl.utils import exceptions
+from edl.utils import train_status as edl_train_status
 from edl.utils import unique_name
+from edl.utils import json_serializable
 from edl.utils.log_utils import logger
-from edl.utils import status as edl_status
+from edl.utils import string_utils
 
 
-class DataCheckpoint(object):
-    def __init__(self, reader_name=None, file_list=None, data_checkpoint=None):
-        self._reader_name = reader_name
-        self._file_list = file_list
-        #file_idx_in_file_list:[(record_idx_begin, record_idx_end), ...]
-        self._data_checkpoint = data_checkpoint
-
-    def to_json(self):
-        pass
-
-    def from_json(self):
-        pass
+class DataCheckpoint(json_serializable.Serializable):
+    def __init__(self, reader_name=None, file_list=None, processed_data=None):
+        self.reader_name = reader_name
+        self.file_list = file_list
+        #dict, file_idx_in_file_list=>[(record_idx_begin, record_idx_end), ...]
+        self.processed_data = processed_data
 
 
-class TrainStatusCheckpoint(object):
-    def __init__(self, max_epoch_num):
-        self._max_epoch_num = max_epoch_num
-        self._epoch_no = None
-        self._epochs = {}
-        self._status = edl_train_status.TrainStatus.INITIAL
+class EpochAttr(json_serializable.Serializable):
+    def __init__(self):
+        self.epoch_no = None
+        self.world_size = None
+        self.step_num = None
+        self.avg_step_time = None
+        self.step_no_of_epoch = None
 
-    def update_epoch(self, epoch_no, step_num, step_time):
-        if epoch_no not in self._epoch:
-            self._epochs[epoch_no] = {}
 
-        self._epochs[epoch_no] = {
-            "epoch_no": epoch_no,
-            "step_num": step_num,
-            "step_time": step_time
-        }
+def _load_dict_of_cls_from_json(json_str, cls):
+    d = json.loads(json_str)
 
-        left_time = (max_epoch_num - epoch_no) * step_num * step_time
-        if left_time > 15 * 60:
-            self._status = edl_train_status.TrainStatus.RUNNING
-        else:
-            self._status = edl_train_status.TrainStatus.NEARTHEEND
+    ret = {}
+    for k, v in six.iteritems(d):
+        ret[int(k)] = cls().from_json(v)
 
-        logger.debug("train status left_time is {} train status is {}".format(
-            left_time))
+    return ret
+
+
+def _dump_dict_to_json(d):
+    ret = {}
+    for k, v in six.iteritems(d):
+        ret[int(k)] = v.to_json()
+
+    return json.dumps(ret)
+
+
+class TrainStatus(json_serializable.Serializable):
+    def __init__(self):
+        self._epoch_no = None  # current
+        self.global_step_no = None  # current
+
+        self._epochs = {}  # epoch_no => EpochAttr
+        self.status = edl_train_status.TrainStatus.INITIAL
 
     def to_json(self):
         d = {
-            "max_epoch_num": self._max_epoch_num,
-            "epoch_no": self._epoch_no,
-            "epochs": self._epochs,
-            "status": int(self._status),
+            "_epoch_no": self._epoch_no,
+            "global_step_no": int(self.global_step_no),
+            "_epochs": _dump_dict_to_json(self._epochs),
+            "status": int(self.status),
         }
 
         return json.dumps(d)
 
-    def from_json(self, s):
-        d = json.loads(s)
-        self._max_epoch_num = d["max_epoch_num"]
-        self._epoch_no = d["epoch_no"]
-        self._epochs = d["epochs"]
-        self._status = d["status"]
+    def from_json(self, json_str):
+        d = json.loads(json_str)
+        self._epoch_no = d["_epoch_no"]
+        self.global_step_no = d["global_step_no"]
+        self.status = d["status"]
 
-    def __str__(self):
-        return self.to_json()
+        print("d[epochs]", d["_epochs"])
+        self._epochs = _load_dict_of_cls_from_json(d["_epochs"], EpochAttr)
+
+    @property
+    def epoch_no(self):
+        return self._epoch_no
+
+    @epoch_no.setter
+    def epoch_no(self, epoch_no):
+        assert epoch_no >= 0
+        if epoch_no not in self._epochs:
+            self._epochs[epoch_no] = {}
+        self._epoch_no = epoch_no
+
+    def get_epoch_attr(self, epoch_no):
+        if epoch_no not in self._epochs:
+            return None
+        return self._epochs[epoch_no]
+
+    def update_epoch_attr(self, epoch_no, epoch_attr):
+        self._epochs[epoch_no] = epoch_attr
+
+    def get_current_epoch_attr(self):
+        return get_epoch_attr(self._epoch_no)
+
+    def update_current_epoch_attr(self, epoch_attr):
+        return self._update_epoch_attr(self._epoch_no, epoch_attr)
 
 
-class State(object):
+class State(json_serializable.Serializable):
     def __init__(self, total_batch_size, user_defined=None):
         # interface
         self._default = {
-            "total_batch_size": total_batch_size,
-            "world_size": None,
-            "epoch_num": None,
-            "epoch_no": None,
-            "global_step_no": None,
-            "step_no_of_epoch": None,
+            "total_batch_size": total_batch_size,  # user inputs
         }
         self._user_defined = user_defined
         self._adjust_func = []
@@ -100,30 +124,39 @@ class State(object):
         self._name = unique_name.generator("_edl_state_")
         self._model_path = None
         self._data_checkpoint = DataCheckpoint()
-        self._train_status = TrainStatusCheckpoint()
+        self._train_status = TrainStatus()
+
+    def from_json(self, json_str):
+        d = json.loads(json_str)
+
+        self._default = d["_default"]
+        if self._user_defined is not None and d["_user_defined"] is not None:
+            self._user_defined.from_json(d["_user_defined"])
+
+        self._name = d["_name"]
+        self._model_path = d["_model_path"]
+        self._data_checkpoint.from_json(d["_data_checkpoint"])
+        self._train_status.from_json(d["_train_status"])
+        return d
 
     def register_adjust_function(self, f):
         self._adjust_func.append(f)
 
     @property
-    def world_size(self):
-        return self._defaults["world_size"]
-
-    @property
-    def epoch_num(self):
-        return self._defaults["epoch_num"]
+    def name(self):
+        return self._name
 
     @property
     def epoch_no(self):
-        return self._defaults["epoch_no"]
+        return self._train_status.epoch_no
 
     @property
     def step_no_of_epoch(self):
-        return self._defaults["step_no_of_epoch"]
+        return self._train_status.get_current_epoch_attr().step_no_of_epoch
 
     @property
     def global_step_no(self):
-        return self._defaults["global_step_no"]
+        return self._train_status.global_step_no
 
     @property
     def total_batch_size(self):
@@ -133,82 +166,35 @@ class State(object):
     def total_batch_size(self, size):
         self._defaults["total_batch_size"] = size
 
-    def to_json(self):
-        d = {
-            "default": json.to_json(self._default),
-            "user_defined": self._user_defined.to_json()
-            if self._user_defined else None,
-            "name": self._name,
-            "model_path": self._model_path,
-            "data_checkpoint": self._data_checkpoint.to_json(),
-            "train_status": self._train_status.to_json(),
-        }
-
-        return json.dumps(d)
-
-    def from_json(self, s):
-        d = json.loads(s)
-
-        self._defaults = d["default"]
-        if self._user_defined is not None and d["user_defined"] is not None:
-            self._user_defined.from_json(d["user_defined"])
-
-        self._name = d["name"]
-        self._model_path = d["model_path"]
-        self._data_checkpoint.from_json(d["data_checkpoint"])
-        self._train_status.from_json(d["train_status"])
-        return d
-
 
 @error_utils.handle_errors_until_timeout
-def load_from_etcd(etcd, state_name, timeout=60):
+def load_from_etcd(etcd, state_name, user_defined=None, timeout=60):
     value = etcd.get_value(constants.ETCD_STATE, state_name)
 
     if value is None:
         raise exceptions.EdlTableError("key:value = {}:{}".format(
-            etcd.get_full_path(constants.ETCD_DIST_READER, state_name), value))
+            etcd.get_full_path(constants.ETCD_READER, state_name), value))
 
-    s = State()
-    s.from_json(value)
-    return s
+    state = State(total_batch_size=None, user_defined=user_defined)
+    state.from_json(string_utils.bytes_to_string(value))
+    return state
 
 
-@error_utils.error_utils.handle_errors_until_timeout
-def save_to_etcd(etcd_endpoints,
-                 job_id,
-                 pod_id,
-                 name,
-                 model_path,
-                 data_checkpoint,
-                 user_defined,
-                 timeout=60):
-    etcd = EtcdClient(
-        endpoints=etcd_endpoints,
-        root=job_id,
-        timeout=constants.EDL_CONN_TIMEOUT)
-    etcd.init()
-
-    s = State()
-    s._name = name
-    s._data_checkpoint = data_checkpoint
-    s._model_path = model_path
-    s._user_defined = user_defined
-
+@error_utils.handle_errors_until_timeout
+def save_to_etcd(etcd, pod_id, state, timeout=60):
     leader_key = etcd.get_full_path(constants.ETCD_POD_RANK,
                                     constants.ETCD_POD_LEADER)
-    state_key = etcd.get_full_path(constants.ETCD_STATE, name)
+    state_key = etcd.get_full_path(constants.ETCD_STATE, state.name)
 
     etcd = etcd._etcd
     status, _ = etcd.transaction(
         compare=[etcd.transactions.value(leader_key) == pod_id, ],
-        success=[etcd.transactions.put(state_key, s.to_json()), ],
+        success=[etcd.transactions.put(state_key, state.to_json()), ],
         failure=[])
 
     message = "pod_id:{} save_data_checkpoint status:{}".format(pod_id, status)
     if not status:
         raise exceptions.EdlEtcdIOError(message)
-
-    return
 
 
 class PaddleState(State):
