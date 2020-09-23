f3b3d2f26671d8ef6a5df4386a7c4b8a64e9f2d8
gongweibao <weibao.gong@gmail.com>
2020-09-23 08:24:03 +0000

    add test=develop

--- python/edl/utils/launcher.py
+++ python/edl/utils/launcher.py
@@ -11,10 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""
-paddle.distributed.launch is a module that spawns multiple distributed
-process on each training node for gpu training.
-"""
 
 from __future__ import print_function
 
